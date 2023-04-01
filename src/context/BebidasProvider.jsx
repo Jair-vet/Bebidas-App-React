4214ad81b430c27a72893058d9b8d46ee6d54f1d
Jair <jairaceves56@gmail.com>
2023-04-01 10:52:03 -0600

    Creamos el Provider de las Bebidas

---
+++ src/context/BebidasProvider.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { createContext } from 'react'
+
+const BebidasContext = createContext()
+const BebidasProvider = ({children}) => {
+    
+
+    return(
+        <BebidasContext.Provider
+            value={{
+                
+            }}
+        >
+            {children}
+        </BebidasContext.Provider>
+    )
+}
+
+export {
+    BebidasProvider
+}
+
+export default BebidasContext
